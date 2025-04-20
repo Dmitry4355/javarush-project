287db8c2fba75c50c0cd04b33baf6e9bd9eda003
Dmitry4355 <feteei@yandex.ru>
2025-04-20 23:01:33 +0300

    Выполнение задачи - task0601-task0609

---
+++ src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,44 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(){
+
+    }
+
+    public static void universalMethod(String str){
+
+    }
+    public static void universalMethod(String str, String x){
+
+    }
+    public static void universalMethod(String str, int x){
+
+    }
+    public static void universalMethod(String str, double five){
+
+    }
+    public static void universalMethod(int a, double q){
+
+    }
+    public static void universalMethod(int a, int b){
+
+    }
+    public static void universalMethod(long t, long c){
+
+    }
+    public static void universalMethod(int b, long c){
+
+    }
+    public static void universalMethod(long h, String str){
+
+    }
+}
