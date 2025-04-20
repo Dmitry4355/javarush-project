3040cfa0a3c6ada3258f3b88bff8481517d017be
Dmitry4355 <feteei@yandex.ru>
2025-04-20 23:01:11 +0300

    Выполнение задачи - task0521

---
+++ src/com/javarush/task/pro/task05/task0521/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task05.task0521;
+
+/* 
+Выполнение фрагмента кода
+*/
+
+public class Solution {
+
+    public static int result = 45967;
+
+    public static void main(String[] args) {
+
+    }
+}
+
